f91e4e88fbc27e46a800cdd255240779b70931b1
25kp <114558445+Karina2137@users.noreply.github.com>
2022-10-02 18:03:21 +0200

    Add files via upload

---
+++ KartaPracy.py
@@ -0,0 +1,24 @@
+
+a = int(input())
+b = int(input())
+c = int(input())
+brutto = int(input())
+
+# Zad.1
+print(a**2+b**2)
+# Zad.2
+print((a+b)**2)
+# Zad.3
+print((a-b)**3)
+# Zad.4
+print(a*b*c)
+# Zad.5
+print(2*(a+b)/5)
+print((a+b)*0.4)
+# Zad.6
+print(brutto/1.23)
+# Zad.7
+print(a%b)
+
+
+
